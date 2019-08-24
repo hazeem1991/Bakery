ccc82a21aa75629a8e0671616ea848a97fff323e
Hazem Arian <hazem@payfull.com>
2019-08-24 22:23:42 +0300

    start with the Package Class

--- src/Models/Package.php
+++ src/Models/Package.php
@@ -4,5 +4,18 @@ namespace Backery\Models\Package;
 
 class Package
 {
-
+    private $count;
+    public function __construct(int $count)
+    {
+        $this->count=$count;
+    }
+    public function getCount():int
+    {
+        return $this->count;
+    }
+    public function setCount(int $count):Package
+    {
+        $this->count=$count;
+        return $this;
+    }
 }
