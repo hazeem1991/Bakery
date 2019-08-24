663e93e2acc5ca93ce92c23313cc775fd2eded6a
Hazem Arian <hazem@payfull.com>
2019-08-24 22:36:26 +0300

    start with the cart Class

--- src/Models/Cart.php
+++ src/Models/Cart.php
@@ -2,7 +2,30 @@
 
 namespace Backery\Models\Cart;
 
-class Cart extends ArrayOject
+class Cart extends \ArrayOject
 {
+    private static $cart;
+    private function __construct()
+    {
+        // Hide the constructor
+    }
+    private function __clone()
+    {
+        // Disable cloning
+    }
 
+    private function __wakeup()
+    {
+        // Disable unserialize
+    }
+
+    public static function newCart(): Cart
+    {
+        if (!self::$cart) {
+            self::$cart = new self();
+        }
+
+        return self::$cart;
+    }
+    
 }
